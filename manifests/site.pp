ca86c6922cf1a7892f84fe7a2cae95fe4a872859
Jakub <30892946+winostworzylbog@users.noreply.github.com>
2021-04-28 12:19:53 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,6 +5,15 @@ node default {
     owner => 'root',
     }
 }
+
 node 'master.puppet.vm' {
   include role::master_server
+}
+
+node /^web/ {
+  include role::app_server
+}
+
+node /^db/ {
+  include role::db_server
 }
